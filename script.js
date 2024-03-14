e9eb0c4da1c2501628d6397b2dcd8cedc311fb02
Matias Barboza <matiasbarboza540@gmail.com>
2024-03-14 19:18:21 -0300

    Finalización maquetado

---
+++ script.js
@@ -0,0 +1,18 @@
+let menuVisible = false;
+
+function mostrarOcultar(){
+    
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    /* Oculta el menú una vez clickeada una opción*/
+    document.getElementById("nav").classList="";
+    menuVisible = false;
+}
